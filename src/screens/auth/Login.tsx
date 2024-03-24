e72f0b3e657c772c148b5ffd2b171b7366599e73
Mo <realmohaa@outlook.com>
2024-03-24 01:04:05 +0100

    Updates Axios

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -9,16 +9,43 @@ import Modal from "react-native-modal";
 import tw from 'twrnc';
 import { Button } from '@rneui/themed';
 import { TextInput } from '../../components/text-input';
+import axios from 'axios';
+
 // Logo Imports
 import logo from '../../assets/images/logo.png';
 import wlogo from '../../assets/images/logowhite.png';
 
 function Login({ navigation }: { navigation: any }): React.JSX.Element {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isloading, setIsLoading] = useState(false);
+
   const [isModalVisible, setModalVisible] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
+
+  const handleLogin = async () => {
+    try {
+      setIsLoading(true)
+      const response = await axios.post('https://192.168.0.165:3000/api/v1/auth/login', {
+        username,
+        password
+      },{
+        headers: { 
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'application/json'
+        }, 
+      });
+      console.log(response.data)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error)
+    }
+  };
+
   return (
     <View
       style={tw`flex flex-col items-center justify-center h-full w-full px-8`}
@@ -29,13 +56,35 @@ function Login({ navigation }: { navigation: any }): React.JSX.Element {
             <Text style={tw`text-2xl text-[#3c403d]`}>Athlete</Text>
             <Image source={wlogo} style={tw`w-[75px] h-[75px]`} />
           </View>
-
           <View style={tw`flex flex-col gap-4 w-full`}>
-            <TextInput style={tw`rounded-full`} placeholder="Username / Email" />
-            <TextInput style={tw`rounded-full`} placeholder="Password" />
+              <TextInput 
+                style={tw`rounded-full`} 
+                placeholder="Username / Email" 
+                value={username}
+                onChangeText={setUsername}
+              />
+              <TextInput 
+                style={tw`rounded-full`} 
+                placeholder="Password" 
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+              />
           </View>
-
-          <Button radius={"xl"} color="white" titleStyle={{padding:28, color:"#55bfa9"}} containerStyle={{marginTop:15}} onPress={toggleModal}>Login</Button>
+          <Button 
+            radius={"xl"} 
+            color="white" 
+            titleStyle={{padding:28, color:"#55bfa9"}} 
+            containerStyle={{marginTop:15}} 
+            onPress={handleLogin}
+            loading={isloading}
+            loadingProps={{
+              size: 'small',
+              color: 'rgba(111, 202, 186, 1)',
+            }}
+            >
+            Login
+          </Button>
         </View>
       </Modal>
       <Image source={logo} style={{ width: 250, height: 250 }} />
